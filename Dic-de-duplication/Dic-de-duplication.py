aee31c8ad00cfadd8661d24ede56856364adfcc7
Malabis <75559487+Malabis@users.noreply.github.com>
2022-03-18 11:37:11 +0800

    Add files via upload

---
+++ Dic-de-duplication/Dic-de-duplication.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+#从键盘输入
+dicfile = sys.argv[1]#源字典1
+addfile = sys.argv[2]#源字典2
+newfile = sys.argv[3]#去重完之后的字典
+
+def merge(dic1,dic2):
+    try:
+        with open(dic1,'a+',encoding = 'utf-8') as f1:
+            with open(dic2,'r',encoding = 'utf-8') as f2:
+                f1.write('\n')
+                for line in f2:#迭代的方式读取文件中的每一行
+                    f1.write(line)
+    except IOError:
+        print("Error: 没有找到文件或读取文件失败")
+
+def deplication(dic):
+    disorder_set = set()#创建一个无序序列
+    try:
+        with open(newfile,'w',encoding='utf-8') as outfile:
+            with open(dic,'r',encoding='utf-8') as f:
+                for line in f:
+                    if line not in disorder_set:
+                        disorder_set.add(line)
+                        outfile.write(line)
+    except IOError:
+        print("Error: 没有找到文件或读取文件失败")
+    else:
+        print("字典去重成功")
+
+
+if __name__ == '__main__':
+    merge(dicfile,addfile)
+    deplication(dicfile)
---
+++ Dic-de-duplication/README.md
@@ -0,0 +1,14 @@
+# python字典去重工具
+
+# Introduce
+
+在渗透测试中必不可少的会遇到爆破时字典太多太冗余的时候，这个时候就需要整理自己字典来更高效的进行工作
+
+## Usage
+
+`python Dic-de-duplication.py filename1 filename2 newfilename`
+
+filename1和filename2是所需要整理的两个原字典，newfilename是整理完之后的自己设定的新的字典名
+
+## Examples
+
